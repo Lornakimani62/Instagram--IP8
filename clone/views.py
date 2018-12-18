004c2f464e9a85db4e338aea6eeb84574f22cf90
lorna nashipae <kimanilorna@yahoo.com>
2018-12-18 17:29:43 +0300

    Adds the profile and image features

--- clone/views.py
+++ clone/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-import datetime as dt
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import logout
+from django.contrib.auth.decorators import login_required
+from .models import *
+from django.shortcuts import redirect
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def home(request):
+    images = Image.objects.all()
+    return render(request, "home.html",)
 
-    date = dt.date.today()
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    images = Image.objects.filter(user=current_user)
 
-
-    return render(request,'registration/login.html')
+    return render(request, "profile.html", {"images":images})
